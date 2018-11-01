d9892bb1d1cf2f2c7d73acb68613db5e9d6634ef
adityapandey1998 <31750960+adityapandey1998@users.noreply.github.com>
2018-11-01 19:49:28 +0530

    Update BowlingDataScraper.py

--- BowlingDataScraper.py
+++ BowlingDataScraper.py
@@ -5,36 +5,44 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-i=0
+itr=0
 player_values=[]
-
-page = requests.get("http://stats.espncricinfo.com/indian-premier-league-2018/engine/records/averages/bowling.html?id=12210;type=tournament")
-soup = BeautifulSoup(page.text, 'html.parser')
-
-player_row = soup.find(class_="engineTable")
-print(player_row)
-
-if i==0:
-    player_row_head = player_row.find("thead")
-    player_row_hval = player_row_head.find_all("th")
-    headers = []
-    for i in player_row_hval:
-        headers.append(i.contents[0])
-    headers.append("Year")
-
-player_row_val = player_row.find("tbody")
-player_row_val = player_row.find_all("tr")
-for i in player_row_val[1::2]:
-    td = i.find_all("td")
-    name = td[0].find("a").contents[0]
-    td2 = list(map(lambda x:x.contents[0],td[1:]))
-    for i in range(len(td2)):
-        try:
-            td2[i]=float(td2[i])
-        except ValueError:
-            td2[i]=td2[i]
-    td2.insert(0,name)
-    td2.append(2018)
-    player_values.append(list(td2))
+links=[]
+with open("Links.txt") as f:
+    links=f.readlines()
+    links = list(map(lambda x:x.rstrip("\n"),links))
+    
+for i in links:
+    page = requests.get(i)
+    print(page)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    #print(soup.prettify())
+    player_row = soup.find(class_="engineTable")
+    #print(player_row)
+    
+    if itr==0:
+        player_row_head = player_row.find("thead")
+        player_row_hval = player_row_head.find_all("th")
+        headers = []
+        for i in player_row_hval:
+            headers.append(i.contents[0])
+        headers.append("Year")
+    
+    player_row_val = player_row.find("tbody")
+    player_row_val = player_row.find_all("tr")
+    for i in player_row_val[1::2]:
+        td = i.find_all("td")
+        name = td[0].find("a").contents[0]
+        td2 = list(map(lambda x:x.contents[0],td[1:]))
+        for i in range(len(td2)):
+            try:
+                td2[i]=float(td2[i])
+            except ValueError:
+                td2[i]=td2[i]
+        td2.insert(0,name)
+        td2.append(2018-itr)
+        player_values.append(list(td2))
+    itr+=1
         
-df2016 = pd.DataFrame(player_values, columns=headers)
+bowlerdf = pd.DataFrame(player_values, columns=headers)
+print(bowlerdf.head())
